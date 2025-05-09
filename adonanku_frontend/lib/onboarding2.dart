525a28c88aeccffdaa25177b370c36c03c725054
Alvin Yuga Pramana <148262832+bigbosspramana@users.noreply.github.com>
2025-05-10 07:08:15 +0800

    Menambahkan boarding page 2,3,4

--- adonanku_frontend/lib/onboarding2.dart
+++ adonanku_frontend/lib/onboarding2.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:lottie/lottie.dart';
-import 'package:flutter/gestures.dart';
-import 'onboarding1.dart';
-// import 'login.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class Onboarding2App extends StatelessWidget {
   const Onboarding2App({super.key});
@@ -26,121 +23,160 @@ class OnBoarding2Page extends StatefulWidget {
   final String title;
 
   @override
-  State<OnBoarding2Page> createState() => _MyHomePageState();
+  State<OnBoarding2Page> createState() => _OnboardingPageViewState();
 }
 
-class _MyHomePageState extends State<OnBoarding2Page>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Offset> _slideLeft;
-  late Animation<Offset> _slideRight;
-  late Animation<double> _fadeIn;
+class _OnboardingPageViewState extends State<OnBoarding2Page> {
+  final PageController _controller = PageController();
+  int currentPage = 0;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(
-        vsync: this, duration: Duration(milliseconds: 800));
-    
-    _slideLeft = Tween<Offset>(begin: Offset(-1, 0), end: Offset.zero)
-        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
-    _slideRight = Tween<Offset>(begin: Offset(1, 0), end: Offset.zero)
-        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
-    _fadeIn = Tween<double>(begin: 0, end: 1)
-        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
-
-    _controller.forward();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+    _controller.addListener(() {
+      setState(() {
+        currentPage = _controller.page!.round();
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-          child: Container(
-              margin: EdgeInsets.only(top: 140),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: <Widget>[
-                      FadeTransition(
-                        opacity: _fadeIn,
-                        child: SlideTransition(
-                          position: _slideLeft,
-                          child: Image.asset('assets/img/boarding3.png'),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          child: Text(
-                            'Selamat Datang',
-                            style: TextStyle(
-                                fontSize: 24, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Container(
-                          width: 310,
-                          child: Text(
-                            'Kami dapat membantu anda dalam mengatur sistem pembuatan kue anda',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                      margin: EdgeInsets.only(top: 30),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => Onboarding1App()), // Changed to HomePage
-                              );
-                            },
-                            style: ElevatedButton.styleFrom(
-                              minimumSize: Size(
-                                  200, 65), // Panjang tombol mengikuti lebar
-                              backgroundColor: Color(
-                                  0xFFCD8636), // Warna latar belakang tombol
-                              foregroundColor:
-                                  Colors.white, // Warna teks tombol
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 20,
-                                  vertical: 15), // Padding dalam tombol
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(
-                                        40)), // Radius sudut tombol
-                              ),
-                            ),
-                            child: Text(
-                              "Masuk",
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          ),
-                        ],
-                      )),
-                ],
-              ))),
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height *
+                0.75, // atau 0.7 kalau mau lebih rapat
+            child: PageView(
+              controller: _controller,
+              children: const [
+                OnboardingPage(
+                  image: 'assets/img/boarding3.png',
+                  title: 'Selamat Datang!',
+                  description:
+                      'Kamu dapat mengatur sistem inventori dengan lebih baik & nyaman!',
+                ),
+                OnboardingPage(
+                  image: 'assets/img/boarding4.png',
+                  title: 'Pantau Stok',
+                  description:
+                      'Temukan berbagai macam resep yang populer dan tambahkan resepmu sendiri!',
+                ),
+                OnboardingPage(
+                  image: 'assets/img/boarding5.png',
+                  title: 'Sistem Inventori',
+                  description:
+                      'Konversikan bahan kue yang kamu miliki menjadi sebuah resep utuh yang kamu butuh!',
+                ),
+              ],
+            ),
+          ),
+
+          // Tombol Lanjut hanya muncul di halaman ke-3
+          if (currentPage == 2)
+            ElevatedButton(
+              onPressed: () {
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //       builder: (context) =>
+                //           Onboarding2App()), // Changed to HomePage
+                // );
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(200,65), // Panjang tombol mengikuti lebar
+                backgroundColor:
+                    Color(0xFFCD8636), // Warna latar belakang tombol
+                foregroundColor: Colors.white, // Warna teks tombol // Padding dalam tombol
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40), bottomRight: Radius.circular(40)), // Radius sudut tombol
+                ),
+              ),
+              child: Text(
+                "Lanjut",
+                style: TextStyle(fontSize: 20),
+              ),
+            )
+          else
+            Container(
+              margin: EdgeInsets.only(top: 17),
+              child: TextButton(
+                onPressed: () {
+                  int nextPage = _controller.page!.toInt() + 1;
+                  if (nextPage < 3) {
+                    _controller.animateToPage(
+                      nextPage,
+                      duration: Duration(milliseconds: 500),
+                      curve: Curves.easeInOut,
+                    );
+                  }
+                },
+                child: Text(
+                  "Skip",
+                  style: TextStyle(fontSize: 18, color: Colors.black),
+                ),
+              ),
+            ),
+          Container(
+            margin: EdgeInsets.only(top: 50),
+            child: SmoothPageIndicator(
+              controller: _controller,
+              count: 3,
+              effect: WormEffect(
+                activeDotColor: Color(0xFFCD8636),
+                dotHeight: 12,
+                dotWidth: 12,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class OnboardingPage extends StatelessWidget {
+  final String image;
+  final String title;
+  final String description;
+
+  const OnboardingPage({
+    required this.image,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.only(top: 130),
+        child: Column(
+          children: [
+            Image.asset(
+              image,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              child: Text(
+                title,
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 30),
+              width: 350,
+              child: Text(
+                description,
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
