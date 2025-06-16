b5e96fb9ed1ef49bd0aec3a155e3b3ed3518c19e
Alvin Yuga Pramana <148262832+bigbosspramana@users.noreply.github.com>
2025-06-16 14:45:26 +0800

    Menambahkan slicing menu dan notif

--- adonanku_frontend/lib/viewmodels/onboarding2_viewmodel.dart
+++ adonanku_frontend/lib/viewmodels/onboarding2_viewmodel.dart
@@ -4,17 +4,17 @@ class OnboardingViewModel {
   List<OnboardingItem> getOnboardingItems() {
     return [
       OnboardingItem(
-        image: 'assets/img/boarding3.png',
+        image: 'assets/img/boarding3.webp',
         title: 'Sistem Inventori',
         description: 'Kamu dapat mengatur sistem inventori dengan lebih baik & nyaman!',
       ),
       OnboardingItem(
-        image: 'assets/img/boarding4.png',
+        image: 'assets/img/boarding4.webp',
         title: 'Resep Populer',
         description: 'Temukan berbagai macam resep yang populer dan tambahkan resepmu sendiri!',
       ),
       OnboardingItem(
-        image: 'assets/img/boarding5.png',
+        image: 'assets/img/boarding5.webp',
         title: 'Konversi Bahan',
         description: 'Konversikan bahan kue yang kamu miliki menjadi sebuah resep utuh yang kamu butuh!',
       ),
